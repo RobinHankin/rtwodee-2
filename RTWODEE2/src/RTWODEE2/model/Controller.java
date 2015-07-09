1edd60d710b8ef21f8f931be5e31e2a3efb55a60
Robin Hankikn <RobinHankin@users.noreply.github.com>
2015-07-10 09:09:13 +1200

    Update Controller.java
    remove honorific

--- RTWODEE2/src/RTWODEE2/model/Controller.java
+++ RTWODEE2/src/RTWODEE2/model/Controller.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 Dr. Robin Hankin
+ * Copyright (C) 2014  Robin Hankin
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
